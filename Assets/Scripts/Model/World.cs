2ab22130340fa7e7d90c8950b3b60536c62e71d9
DylanFoss <foss.dylan.00@gmail.com>
2021-10-19 15:03:38 +0100

    Renamed WorldData to Data: - World.WorldData is a bit much.

--- Assets/Scripts/Model/World.cs
+++ Assets/Scripts/Model/World.cs
@@ -12,7 +12,7 @@ public class World
 
     private TerrainGenerator terrainGenerator;
     public AnimalManager AnimalManager { get; protected set; }
-    private WorldData worldData;
+    public WorldData Data { get; protected set;}
 
     // TODO: Move this to the struct that Dylan will add
     public List<Tile> FoodTiles { get; protected set; }
@@ -25,7 +25,7 @@ public class World
         FoodTiles = new List<Tile>();
         terrainGenerator = new TerrainGenerator();
         AnimalManager = new AnimalManager(this);
-        worldData = new WorldData();
+        Data = new WorldData();
         for (int x = 0; x < Width; x++)
         {
             for (int y = 0; y < Height; y++)
@@ -52,7 +52,7 @@ public class World
 
     public void GenerateTerrain()
     {
-        worldData.TerrainData = terrainGenerator.GenerateTerrain(tiles);
+        Data.TerrainData = terrainGenerator.GenerateTerrain(tiles);
     }
 
     public void SpawnAnimals(int preyAmount, int predatorAmount)
