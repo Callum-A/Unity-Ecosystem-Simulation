0d898e32ac77a581f05db419e7a9a2a8de71d2bc
orran syms <orran@live.co.uk>
2021-10-27 01:44:17 +0100

    -Removing log from world controller. -Adding World log method to worldController. -Assigning world log method to the time contoller  callback.

--- Assets/Scripts/Controllers/WorldController.cs
+++ Assets/Scripts/Controllers/WorldController.cs
@@ -65,12 +65,23 @@ public class WorldController : MonoBehaviour
         World.GenerateTerrain();
         World.SproutInitialFood();
         World.SpawnAnimals(10, 0);
-        EventLogController.AddLog("Simulation started!");
+        //EventLogController.AddLog("Simulation started!");
+        //EventLogController.AddLog("Initial Food: " + World.getFoodTiles().Count + " Total Nutrition: " + World.getTotalNutritionOnMap());
+        //EventLogController.AddLog("Initial Prey: " + World.getPrey().Count);
+    }
+
+    public void WorldCountLog() 
+    {
+        EventLogController.AddLog($"Day {TimeController.Instance.NumberOfDays} started!");
+        EventLogController.AddLog("Current Food: " + World.getFoodTiles().Count);
+        EventLogController.AddLog("Current Nutrition: " + World.getTotalNutritionOnMap());
+        EventLogController.AddLog("Current Prey: " + World.getPrey().Count);
     }
 
     private void Start()
     {
         TimeController.Instance.RegisterOnNewDayCallback(World.FoodManager.OnNewDay);
+        TimeController.Instance.RegisterOnNewDayCallback(o => WorldCountLog());
     }
 
 }
