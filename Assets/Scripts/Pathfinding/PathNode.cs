2dd9a5152974d03477fe7dcdd2437045cf9cc4b4
DylanFoss <foss.dylan.00@gmail.com>
2021-11-17 22:36:46 +0000

    removed pointless commented out code

--- Assets/Scripts/Pathfinding/PathNode.cs
+++ Assets/Scripts/Pathfinding/PathNode.cs
@@ -6,15 +6,5 @@ public class PathNode<T>
 {
     public T data;
     public PathEdge<T>[] edges;
-
-    //public override bool Equals(object obj)
-    //{
-    //    return data.Equals(obj);
-    //}
-
-    //public override int GetHashCode()
-    //{
-    //    return data.GetHashCode();
-    //}
 }
  
